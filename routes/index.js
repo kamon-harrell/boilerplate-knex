95ca6dfd49595aebd630665b1c10d9ba7c188c12
Sach <shsaci@yahoo.com.au>
2016-08-04 11:11:30 +1200

    got profiles clickthroughs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ var express = require('express')
 var development = require('../knexfile').development
 var knex = require('knex')(development)
 
-module.exports = {
-  get: get
-}
 
 function get (req, res) {
   knex('users')
@@ -15,4 +12,26 @@ function get (req, res) {
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
+}
+
+function profiles(req, res) {
+  var userId = req.params
+  knex('users')
+    .join('profiles', 'user_id', '=', 'users.id')
+    .select()
+    .then(function (data) {
+      for(var i = 0; i < data.length; i++) {
+        if (data[i].user_id == userId.id) {
+          res.render('profiles', {profiles: data[i]})
+        }
+      }
+    })
+    .catch(function(err) {
+      res.status(500).send('THUS IS A PA TA TO DATA BASE ' + err.message)
+    })
+}
+
+module.exports = {
+  get: get,
+  profiles: profiles
 }
